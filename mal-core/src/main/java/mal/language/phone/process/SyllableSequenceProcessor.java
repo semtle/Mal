a7e4787e4f7f338d90ff1d91b9aaf95d442e0702
Teddy Choi <pudidic@gmail.com>
2012-08-06 22:08:14 +0900

    Refactoring

--- mal-core/src/main/java/mal/language/phone/process/SyllableSequenceProcessor.java
+++
@@ -1,9 +0,0 @@
-package mal.language.phone.process;
-
-import mal.language.letter.Syllable;
-
-import java.util.List;
-
-public interface SyllableSequenceProcessor {
-    List<Syllable> process(List<Syllable> syllables);
-}
